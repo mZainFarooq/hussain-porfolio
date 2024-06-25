e102266bd8d2efb1f4a4405d5d24755ff7252227
M.ZAIN FAROOQ <zainfarr47@gmail.com>
2024-06-25 12:53:04 +0500

    first-commit

---
+++ src/data/projects.js
@@ -0,0 +1,69 @@
+// Import images
+import CM from '../assets/CM.png';
+import SI from '../assets/SI.png';
+import sendmea from '../assets/sendmea.png';
+import andymation from '../assets/andymation(1).png';
+import tax1 from '../assets/tax1.png';
+import ai from '../assets/ai2.png';
+import ai3 from '../assets/ai3.png';
+import lead1 from '../assets/lead1.png';
+
+
+export const projectsData = [
+	{
+		id: 1,
+		title: 'Send Mea',
+		category: 'Web Application',
+		img: sendmea,
+		link:'https://sendmea.io/',
+	},
+	{
+		id: 2,
+		title: 'Screen Indie',
+		category: 'Web Application',
+		img: SI,
+		link:'https://screenindie.com/'
+	},
+	{
+		id: 3,
+		title: 'Credential Mangement',
+		category: 'Web Application',
+		img: CM,
+		link:'https://getcredmanagement.com/'
+	},
+	{
+		id: 4,
+		title: 'Andymation',
+		category: 'Mobile & Web Application',
+		img: andymation,
+		link:'https://play.google.com/store/apps/details?id=com.andymation'
+	},
+	{
+		id: 5,
+		title: 'Tax',
+		category: 'Web Application',
+		img: tax1,
+		link:'https://tax.americanrefundrecovery.com/'
+	},
+	{
+		id: 6,
+		title: 'Ai',
+		category: 'Web Application',
+		img: ai,
+		link:'https://ai-dev-tools-b1258.web.app/'
+	},
+	{
+		id: 7,
+		title: 'Ai Study Flow',
+		category: 'Web Application',
+		img: ai3,
+		link:'https://aistudyflow.com/'
+	},
+	{
+		id: 8,
+		title: 'Lead Magnet IQ',
+		category: 'Web Application',
+		img: lead1,
+		link:'https://leadmagnetiq.com/'
+	},
+];
